f301fb82fc2984c76b1b076f5348fcbe0f92fbb1
nugi <nugi.1993@gmail.com>
2015-09-01 09:33:59 +0700

    Update sidebar

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -8,7 +8,7 @@
                     <span class="role">Super Admin</span>
                 @elseif (auth()->user()->role == 'finance')
                     <span class="role">Finance</span>
-                @elseif (auth()->user()->role == 'administation')
+                @elseif (auth()->user()->role == 'administration')
                     <span class="role">Administation</span>
                 @else
                     <span class="role">Employee</span>
@@ -16,21 +16,37 @@
             </div>
         </div>
         <ul class="sidebar-menu">
-            <li>
-                <a href="pages/widgets.html">
+            <li{{ (Request::is('dashboard') ? ' class=active' : '')}}>
+                <a href="/dashboard">
                     <i class="fa fa-fw fa-th"></i> <span>Dashboard</span>
                 </a>
             </li>
             @if (auth()->user()->role == 'super_admin')
-                <li class="treeview item-active">
+                <li class="treeview{{ (
+                            Request::is('user*') ||
+                            Request::is('kota*') ||
+                            Request::is('prospek*') ||
+                            Request::is('project*')
+                        )
+                        ? ' item-active' : ''
+                    }}"
+                >
                     <a href="#">
                         <i class="fa fa-fw fa-database"></i> <span>Manage Data</span> <i class="fa fa-angle-down pull-right"></i>
                     </a>
                     <ul class="treeview-menu menu-open" style="display: block;">
-                        <li><a href="/user"><i class="fa fa-fw"></i>Data User</a></li>
-                        <li><a href="/kota"><i class="fa fa-fw"></i>Data Kota</a></li>
-                        <li><a href="/prospek"><i class="fa fa-fw"></i>Data Prospek</a></li>
-                        <li><a href="/project"><i class="fa fa-fw"></i>Data Project</a></li>
+                        <li{{ (Request::is('user*') ? ' class=item-active' : '')}}>
+                            <a href="/user"><i class="fa fa-fw"></i>Data User</a>
+                        </li>
+                        <li{{ (Request::is('kota*') ? ' class=item-active' : '')}}>
+                            <a href="/kota"><i class="fa fa-fw"></i>Data Kota</a>
+                        </li>
+                        <li{{ (Request::is('prospek*') ? ' class=item-active' : '')}}>
+                            <a href="/prospek"><i class="fa fa-fw"></i>Data Prospek</a>
+                        </li>
+                        <li{{ (Request::is('project*') ? ' class=item-active' : '')}}>
+                            <a href="/project"><i class="fa fa-fw"></i>Data Project</a>
+                        </li>
                     </ul>
                 </li>
             @endif
