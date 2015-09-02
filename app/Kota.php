0ba69a8a9d67e091d6671fa3be830e02c2f39892
esaisyah <euis.s.aisyah@gmail.com>
2015-09-02 16:23:37 +0700

    Soft Delete & Maxlenght
    Menambahkan migration tambah kolom untuk softdelete.
    Mengubah model pegawai, kota, pelatihan, project, prospek
    Menambahkan maxlenght untuk nik pegawai

--- app/Kota.php
+++ app/Kota.php
@@ -3,11 +3,21 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Kota extends Model
 {
+	use SoftDeletes;
+
     protected $table = 'kota';
     protected $fillable = ['nama_kota'];
 
     public $primaryKey = 'kode';
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
 }
--- app/Pelatihan.php
+++ app/Pelatihan.php
@@ -3,11 +3,21 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Pelatihan extends Model
 {
+	use SoftDeletes;
+
     protected $table = 'pelatihan';
     protected $fillable = ['nama_pelatihan','nama_lembaga','tanggal_mulai','tanggal_selesai','alamat'];
 
     public $primaryKey = 'kode';
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
 }
