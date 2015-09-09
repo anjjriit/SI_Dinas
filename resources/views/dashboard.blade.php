2910d3755c1bcda4519aeb1b20d050d08808eacd
esaisyah <euis.s.aisyah@gmail.com>
2015-09-09 10:31:09 +0700

    tambah donat chart dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -23,7 +23,8 @@
 						<h3 class="box-title">Prospek</h3>
 					</div>
 					<div class="box-body">
-						dddd
+						<input type="text" class="knob" value="80" data-skin="tron"  data-thickness="0.2" data-width="120" data-height="120" data-fgColor="#3c8dbc" data-readonly="true" />
+	                    <div class="knob-label">prospek</div>
 					</div>
 					<div class="box-footer">
 						sss
@@ -36,7 +37,10 @@
 						<h3 class="box-title">Proyek dan Pelatihan</h3>
 					</div>
 					<div class="box-body no-padding">
-						
+						<input type="text" class="knob" value="80" data-skin="tron"  data-thickness="0.2" data-width="90" data-height="90" data-fgColor="#3c8dbc" data-readonly="true" />
+	                    <div class="knob-label">proyek</div>
+	                    <input type="text" class="knob" value="80" data-skin="tron"  data-thickness="0.2" data-width="90" data-height="90" data-fgColor="#3c8dbc" data-readonly="true" />
+	                    <div class="knob-label">pelatihan</div>
 					</div>
 					<div class="box-footer">
 						sss
@@ -49,7 +53,8 @@
 						<h3 class="box-title">User</h3>
 					</div>
 					<div class="box-body">
-						
+						<input type="text" class="knob" value="80" data-skin="tron"  data-thickness="0.2" data-width="120" data-height="120" data-fgColor="#3c8dbc" data-readonly="true" />
+	                    <div class="knob-label">user</div>                    
 					</div>
 					<div class="box-footer">
 						sss
@@ -70,6 +75,7 @@
 					</ul>
 					<div class="tab-content">
 						<div role="tabpanel" id="data_rpd" class="tab-pane active" style="position : relative">
+							<h3 class="text-center">Data Rencana Perjalanan Dinas</h3>
 							<table class="table">
 	    						<thead>
 	    							<tr>
@@ -108,5 +114,70 @@
 
 @section('script')
 	@parent
+
+	<script type="text/javascript" src="/vendor/jquery/jquery.knob.js"></script>
+
+	<script type="text/javascript">
+		$(function () {
+	        /* jQueryKnob */
+
+	        $(".knob").knob({
+	          /*change : function (value) {
+	           //console.log("change : " + value);
+	           },
+	           release : function (value) {
+	           console.log("release : " + value);
+	           },
+	           cancel : function () {
+	           console.log("cancel : " + this.value);
+	           },*/
+	          draw: function () {
+
+	            // "tron" case
+	            if (this.$.data('skin') == 'tron') {
+
+	              var a = this.angle(this.cv)  // Angle
+	                      , sa = this.startAngle          // Previous start angle
+	                      , sat = this.startAngle         // Start angle
+	                      , ea                            // Previous end angle
+	                      , eat = sat + a                 // End angle
+	                      , r = true;
+
+	              this.g.lineWidth = this.lineWidth;
+
+	              this.o.cursor
+	                      && (sat = eat - 0.3)
+	                      && (eat = eat + 0.3);
+
+	              if (this.o.displayPrevious) {
+	                ea = this.startAngle + this.angle(this.value);
+	                this.o.cursor
+	                        && (sa = ea - 0.3)
+	                        && (ea = ea + 0.3);
+	                this.g.beginPath();
+	                this.g.strokeStyle = this.previousColor;
+	                this.g.arc(this.xy, this.xy, this.radius - this.lineWidth, sa, ea, false);
+	                this.g.stroke();
+	              }
+
+	              this.g.beginPath();
+	              this.g.strokeStyle = r ? this.o.fgColor : this.fgColor;
+	              this.g.arc(this.xy, this.xy, this.radius - this.lineWidth, sat, eat, false);
+	              this.g.stroke();
+
+	              this.g.lineWidth = 2;
+	              this.g.beginPath();
+	              this.g.strokeStyle = this.o.fgColor;
+	              this.g.arc(this.xy, this.xy, this.radius - this.lineWidth + 1 + this.lineWidth * 2 / 3, 0, 2 * Math.PI, false);
+	              this.g.stroke();
+
+	              return false;
+	            }
+	          }
+	        });
+	        /* END JQUERY KNOB */
+
+        });
+	</script>
 
 @endsection
