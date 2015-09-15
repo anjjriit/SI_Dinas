160182c9ceeaa8149f8f445da93a9ca9d206f98c
nugi <nugi.1993@gmail.com>
2015-09-15 22:13:25 +0700

    edit detail rpd

--- resources/views/rpd/submitted.blade.php
+++ resources/views/rpd/submitted.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.master')
 
-@section('page_title', 'Home')
+@section('page_title', 'RPD')
 
 @section('stylesheet')
 	@parent
@@ -8,26 +8,6 @@
 @endsection
 
 @section('content')
-
-	@if (session('success'))
-		<div class="content">
-			<div class="row">
-                <div class="alert alert-success">
-                    {{ session('success') }}
-                </div>
-            </div>
-		</div>
-    @endif
-
-    @if (session('error'))
-    	<div class="content">
-			<div class="row">
-                <div class="alert alert-danger">
-                    {{ session('error') }}
-                </div>
-            </div>
-		</div>
-    @endif
 
 	<section class="content-header">
         <h1>Data Rencana Perjalanan Dinas</h1>
@@ -38,7 +18,19 @@
     	<div class="row">
     		<!--Bagian box tabel-->
     		<div class="col-md-12">
-    			@if ($submittedRpds->count() != 0)
+    			@if (session('success'))
+                    <div class="alert alert-success">
+                        {{ session('success') }}
+                    </div>
+                @endif
+
+                @if (session('error'))
+                    <div class="alert alert-danger">
+                        {{ session('error') }}
+                    </div>
+                @endif
+
+                @if ($submittedRpds->count() != 0)
 	    			<div class="box box-widget">
 	    				<div class="box-body no-padding">
 	    					<table class="table">
@@ -86,29 +78,29 @@
                     </div>
     			@endif
 
-
+                {!! $submittedRpds->render() !!}
     		</div>
     	</div>
     </section>
 
-    
+
     <!-- Bagian Modal Detail RPD-->
     @foreach ($submittedRpds as $rpd)
 	<div class="modal fade" id="detailRPD-{{ $rpd->id }}" tabindex="-1" role="dialog" aria-labelledby="detailRPDLabel">
-		<div class="modal-dialog" role="document">
+		<div class="modal-dialog modal-lg" role="document">
 			<div class="modal-content">
 				<div class="modal-header">
 					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-					<h1 class="modal-title" id="myModalLabel">Rencana Perjalanan Dinas (RPD)</h1>
+					<h4 class="modal-title" id="myModalLabel">Rencana Perjalanan Dinas (RPD)</h4>
 				</div>
 				<div class="modal-body">
 					<div>
 						<!-- Info basic dari RPD -->
-                        <table class="table table-modal table-responsive">
+                        <table class="table table-modal table-responsive table-condensed">
                             <tbody>
                                 <tr>
-                                    <th class="col-md-4">ID</th>
-                                    <td>{{ $rpd->id }}</td>
+                                    <th class="col-md-4">Kode RPD</th>
+                                    <td>{{ $rpd->kode }}</td>
                                 </tr>
                                 <tr>
                                     <th class="col-md-4">Penanggung Jawab</th>
@@ -116,28 +108,24 @@
                                 </tr>
                                 <tr>
                                     <th class="col-md-4">Kategori</th>
-                                    <td>{{ $rpd->kategori }}</td>
+                                    <td>{{ ucwords(str_replace('_', ' ', $rpd->kategori)) }}</td>
                                 </tr>
                                 <tr>
                                     <th class="col-md-4">Jenis</th>
-                                    <td>{{ $rpd->jenis_perjalanan }}</td>
+                                    <td>{{ ucwords(str_replace('_', ' ', $rpd->jenis_perjalanan)) }}</td>
                                 </tr>
                                 <tr>
                                     <th class="col-md-4">Tanggal Mulai</th>
-                                    <td>{{ $rpd->tanggal_mulai }}</td>
+                                    <td>{{ date_format( date_create($rpd->tanggal_mulai), 'd/m/Y') }}</td>
                                 </tr>
                                 <tr>
                                     <th class="col-md-4">Tanggal Selesai</th>
-                                    <td>{{ $rpd->tanggal_selesai }}</td>
+                                    <td>{{ date_format( date_create($rpd->tanggal_selesai), 'd/m/Y') }}</td>
                                 </tr>
                                 <tr>
                                     <th class="col-md-4">Jumlah Hari Dinas</th>
                                     <td>
-                                    	{{ date_diff(
-										        date_create($rpd->tanggal_mulai),
-										        date_create($rpd->tanggal_selesai)
-											)->d 
-										}}
+                                    	{{ $rpd->lama_hari }} hari
                                     </td>
                                 </tr>
                                 <tr>
@@ -153,14 +141,14 @@
                                     <td>
                                     	<ul>
                                     		@foreach($rpd->saranaTransportasi as $saranaTransportasi)
-	                                    		<li>{{ $saranaTransportasi->nama_transportasi }}</li> 
+	                                    		<li>{{ $saranaTransportasi->nama_transportasi }}</li>
 	                                    	@endforeach
 	                                    </ul>
-                                    </td>                                    	
+                                    </td>
                                 </tr>
                                 <tr>
                                     <th class="col-md-4">Sarana Penginapan</th>
-                                    <td>{{ $rpd->sarana_penginapan }}</td>
+                                    <td>{{ $rpd->saranaPenginapan->nama_penginapan }}</td>
                                 </tr>
                                 <tr>
                                     <th class="col-md-4">Status</th>
@@ -181,11 +169,25 @@
 							</thead>
 							<tbody>
 								@foreach($rpd->peserta as $peserta)
-									<tr>
-										<td>{{ $peserta->nama_lengkap }}</td>
-										<td>{{ $peserta->pivot->jenis_kegiatan }}</td>
-										<td>{{ $peserta->pivot->kegiatan }}</td>
-									</tr>
+                                    @foreach($rpd->kegiatan()->where('nik_peserta', $peserta->nik)->get() as $kegiatan)
+    									<tr>
+                                            @if ($rpd->kegiatan()->where('nik_peserta', $peserta->nik)->first() == $kegiatan)
+        										<td rowspan="{{ $rpd->kegiatan()->where('nik_peserta', $peserta->nik)->count() }}" style="vertical-align: top;">
+                                                    {{ $peserta->nama_lengkap }}
+                                                </td>
+                                            @endif
+    										<td>
+                                                @if ($kegiatan->jenis_kegiatan == 'project')
+                                                    {{ $kegiatan->project->nama_project }}
+                                                @elseif ($kegiatan->jenis_kegiatan == 'prospek')
+                                                    {{ $kegiatan->prospek->nama_prospek }}
+                                                @elseif ($kegiatan->jenis_kegiatan == 'pelatihan')
+                                                    {{ $kegiatan->pelatihan->nama_pelatihan }}
+                                                @endif
+                                            </td>
+    										<td>{{ ucwords(strtolower(str_replace('_', ' ', $kegiatan->kegiatan))) }}</td>
+    									</tr>
+                                    @endforeach
 								@endforeach
 							</tbody>
 						</table>
@@ -209,7 +211,7 @@
 							<tbody>
 								@foreach($rpd->actionHistory as $action)
 									<tr>
-										<td>{{ $action->updated_at }}</td>
+										<td>{{ date_format( date_create($action->created_at), 'd/m/Y H:i') }}</td>
 										<td>{{ $action->pegawai->nama_lengkap }}</td>
 										<td>{{ $action->action }}</td>
 									</tr>
